b9c6efa000117e5ea1d29eca05369ab84fc00309
Cris-689 <cristian.artero.mol@gmail.com>
2025-11-12 11:55:57 +0100

    meh

---
+++ KARRATS/dockerfiles/base/admin/usuarios/usuarios.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# carga las variables de entono pasadas desde el DockerCompose
+set -e
+
+checkUsuario(){
+    if grep -q "${USUARIO}" /etc/passwd; then
+        echo "El usuario ${USUARIO} existe en el sistema." >> /root/logs/informe.log
+        return 1
+    else
+        echo "El usuario ${USUARIO} no existe en el sistema." >> /root/logs/informe.log
+        return 0
+    fi
+}
+checkHome(){
+    if [ ! -d "/home/${USUARIO}" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+newUser(){
+    checkUsuario
+    if [ "$?" -eq 0 ]; then
+        checkHome
+        if [ "$?" -eq 0 ]; then
+            useradd -rm -d /home/${USUARIO} -s /bin/bash ${USUARIO}
+            echo "${USUARIO}:${PASSWORD}" | chpasswd
+            echo "Bienvenido ${USUARIO} a tu empresa ..." > /home/${USUARIO}/bienvenida.txt
+            echo "Usuario ${USUARIO} creado con éxito." >> /root/logs/informe.log
+            echo "/home/${USUARIO} ya existe." >> /root/logs/informe.log
+        else
+            echo "No se pudo crear el usuario ${USUARIO} porque el directorio home ya existe." >> /root/logs/informe.log
+        fi
+    fi
+}
