e1ac78821cff7627c54418f78d7f02ebb4e5fa3d
Cris-689 <cristian.artero.mol@gmail.com>
2025-11-26 11:56:33 +0100

    al final si eran comillas

--- KARRATS/dockerfiles/base/admin/ssh/MainSSH.sh
+++ KARRATS/dockerfiles/base/admin/ssh/MainSSH.sh
@@ -4,8 +4,8 @@ set -e
 
 make_ssh(){
     # cambiar puerto ssh
-    sed -i 's/#Port 22/Port ${SSH_PORT}/' /etc/ssh/sshd_config
-    
+    sed -i "s/#Port 22/Port ${SSH_PORT}/" /etc/ssh/sshd_config
+
     sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
     mkdir -p /home/${USUARIO}/.ssh
