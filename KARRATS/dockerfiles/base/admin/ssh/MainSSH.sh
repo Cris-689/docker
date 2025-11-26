1b7a7a188543fad104a49cd3437bea9c9701c943
Cris-689 <cristian.artero.mol@gmail.com>
2025-11-26 12:09:43 +0100

    Creo que funciona?

--- KARRATS/dockerfiles/base/admin/ssh/MainSSH.sh
+++ KARRATS/dockerfiles/base/admin/ssh/MainSSH.sh
@@ -3,8 +3,7 @@ make_ssh() {
     sed -i 's/#Port 22/Port '$SSH_PORT'/' /etc/ssh/sshd_config
     sed -i 's/#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
 
-    if [ ! -d /home/$USUARIO/.ssh]
-    then
+    if [ ! -d /home/$USUARIO/.ssh ]; then
     mkdir /home/$USUARIO/.ssh
     #cp /root/admin/common/* /home/$USUARIO/.ssh/
     cat /root/admin/common/id_rsa.pub > /home/$USUARIO/.ssh/authorized_keys
