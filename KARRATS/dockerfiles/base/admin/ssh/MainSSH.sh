023e9178d516d01fa038f2cd1e76d573ecf32ed4
Cris-689 <cristian.artero.mol@gmail.com>
2025-11-26 12:14:09 +0100

    :sigh:

--- KARRATS/dockerfiles/base/admin/ssh/MainSSH.sh
+++ KARRATS/dockerfiles/base/admin/ssh/MainSSH.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-make_ssh() {
-    sed -i 's/#Port 22/Port '$SSH_PORT'/' /etc/ssh/sshd_config
-    sed -i 's/#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+# carga las variables de entono pasadas desde el DockerCompose
+set -e
 
-    if [ ! -d /home/$USUARIO/.ssh ]; then
-    mkdir /home/$USUARIO/.ssh
-    #cp /root/admin/common/* /home/$USUARIO/.ssh/
-    cat /root/admin/base/common/id_rsa.pub > /home/$USUARIO/.ssh/authorized_keys
-    fi
+make_ssh(){
+    # cambiar puerto ssh
+    sed -i "s/#Port 22/Port ${SSH_PORT}/" /etc/ssh/sshd_config
 
-    exec /usr/sbin/sshd
+    sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
+    mkdir -p /home/${USUARIO}/.ssh
+   # cp /root/admin/base/common/id_ed25519.pub /home/${USUARIO}/.ssh
+    cat /root/admin/base/common/id_rsa.pub >> /home/${USUARIO}/.ssh/authorized_keys
+    exec /usr/sbin/sshd -D &
 }
